16955d178ffc01be95414a2aa6e94df56ee8c977
Dariusz Lipinski <daro180@icloud.com>
2024-11-20 17:31:35 +0100

    cube

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -17,8 +17,38 @@ const Main = ({user}) => {
     return(
         <section id='main'>
             <Contacts user={user}/>
-            <News/>
+            <News user={user}/>
+            <Cube/>
         </section>
+    )
+}
+
+const Signature = () => {
+    return(
+    <footer id='signature'>
+        <span>Created by Dariusz Lipiński</span>
+    </footer>
+    )
+}
+
+const Cube = () => {
+    return(
+    <aside id="something">   
+        <div className="cubeLine"></div>
+        <div id="placeForCube">
+            <div id="stageForCube">
+                <div id="cube">
+                    <div className="wall front">MB</div>
+                    <div className="wall back"></div>
+                    <div className="wall left"></div>
+                    <div className="wall right"></div>
+                    <div className="wall top"></div>
+                    <div className="wall bottom"></div>
+                </div>
+            </div>
+        </div>  
+        <div className="cubeLine"></div>
+    </aside>
     )
 }
 
@@ -29,6 +59,7 @@ export const MainContent = (props) => {
         <section id='container'>
             <Top onLogout={onLogout}/>
             <Main user={user}/>
+            <Signature/>
         </section>
     )
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react'
 import './News.css'
 
-export const News = () => {
+export const News = ({user}) => {
+    const [news, setNews] = useState([
+        {login: "Mufasa", text: "Pamiętacie o urodzinach Rafiki?"}
+    ])
+ 
+    const newsShare = (e) => {
+        const text = e.target.parentNode.parentNode.firstChild.firstChild.value;
+        e.target.parentNode.parentNode.firstChild.firstChild.value = "";
+        const message = {login: user.name, text: text}
+    }
+
+
+
     return(
         <section id="news">
-
+            <form id="newsShare">
+                <div>
+                    <textarea id="newsText" placeholder="What do you think?" name="reg-pw"></textarea>
+                </div>
+                <div>
+                    <input id='shareButton' type='button' value="Share" onClick={newsShare}/>
+                </div>         
+            </form>
+            <ul>
+                {news.map((n) => (<li><article><h3>{n.login} shared:</h3><div>{n.text}</div></article></li>))}
+            </ul>  
         </section>
     )
 }
